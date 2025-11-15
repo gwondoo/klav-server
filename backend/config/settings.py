65bc8f1c26ab7f9f50e0df4913a058f859298b34
gwondoo <6568kdh@naver.com>
2025-11-15 10:56:32 +0900

    refactor: change database settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -81,14 +81,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "klav",
+        # "USER": "klav_user",
+        # "PASSWORD": "klav_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -130,17 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "klav",
-        # "USER": "klav_user",
-        # "PASSWORD": "klav_password",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
