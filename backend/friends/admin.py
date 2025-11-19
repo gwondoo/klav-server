16c22d6d7c9b179002b985b3dc245427cafd348c
gwondoo <6568kdh@naver.com>
2025-11-20 05:52:35 +0900

    feat: update main

--- backend/friends/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Follow
-
-
-@admin.register(Follow)
-class FollowAdmin(admin.ModelAdmin):
-    list_display = ("id", "follower", "following", "created_at")
-    search_fields = ("follower__username", "following__username")
--- backend/profiles/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "nickname", "created_at")
-    search_fields = ("nickname", "user__username")
